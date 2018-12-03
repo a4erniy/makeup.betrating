5171fbf34ff0364a9652244bf5c6284ae90280fa
a_cherniy <aleksandr.cherniy@gmail.com>
2018-12-03 20:16:02 +0300

    Initial commit

---
+++ index.php
@@ -0,0 +1,54 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="stylesheet" href="/assets/css/bootstrap.min.css">
+    <link rel="stylesheet" href="/assets/css/style.css">
+    <title>Betrating.ru</title>
+</head>
+<body>
+
+<header>
+    <div class="container">
+        <div class="header">
+            <div class="logo">
+                <a href="/">
+                    <img src="assets/image/logo.png" alt="">
+                </a>
+            </div>
+            <div class="general-menu">
+                <ul>
+                    <li><a href="#">Новости</a></li>
+                    <li><a href="#">Рейтинг БК</a></li>
+                    <li><a href="#">Прогнозы</a></li>
+                    <li><a href="#">Школа ставок</a></li>
+                    <li><a href="#">Бонусы</a></li>
+                    <li><a href="#">Премиум</a></li>
+                </ul>
+            </div>
+            <div class="search-bar"></div>
+            <div class="login"></div>
+            <div class="search-button">
+            </div>
+        </div>
+    </div>
+</header>
+
+<section class="top-area"></section>
+<section class="forecast-area"></section>
+<section class="school-area"></section>
+<section class="school-area"></section>
+<section class="news-area"></section>
+<section class="bonuses-area"></section>
+<section class="stavkatv-area"></section>
+
+<footer></footer>
+
+<script type="text/javascript" src="/assets/js/jquery.min.js"></script>
+<script type="text/javascript" src="/assets/js/popper.min.js"></script>
+<script type="text/javascript" src="/assets/js/bootstrap.min.js"></script>
+<script type="text/javascript" src="/assets/js/scripts.js"></script>
+
+</body>
+</html>
