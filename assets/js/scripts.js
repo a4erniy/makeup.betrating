281561c6248b19502920e67ef202e463ebe5c90c
a_cherniy <aleksandr.cherniy@gmail.com>
2018-12-06 20:25:30 +0300

    In progress

---
+++ assets/js/scripts.js
@@ -0,0 +1,9 @@
+$(function () {
+
+//    Search in header
+    $('#search_button').on('click', function (e) {
+        e.preventDefault();
+       $(this).toggleClass('open')
+    });
+
+});
--- index.php
+++ index.php
@@ -20,16 +20,20 @@
             <div class="general-menu">
                 <ul>
                     <li class="active"><a href="#">Новости</a></li>
-                    <li class=""><a href="#">Рейтинг БК</a></li>
-                    <li class=""><a href="#">Прогнозы</a></li>
+                    <li class=""><a href="#">Рейтинг БК<span></span></a></li>
+                    <li class=""><a href="#">Прогнозы<span></span></a></a></li>
                     <li class=""><a href="#">Школа ставок</a></li>
                     <li class=""><a href="#">Бонусы</a></li>
-                    <li class=""><a href="#">Премиум</a></li>
+                    <li class="premium"><a href="#"><span></span>Премиум</a></li>
                 </ul>
             </div>
             <div class="search-bar"></div>
             <div class="login"></div>
             <div class="search-button">
+                <a href="#" class="" id="search_button">
+                    <span class="loop"><img src="assets/image/svg/action/search_24px.svg"></span>
+                    <span class="cross"><img src="assets/image/svg/content/clear_24px.svg"></span>
+                </a>
             </div>
         </div>
     </div>
