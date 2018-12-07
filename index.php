c7e57a538dca454e4429fda7959b7296272b51c6
Aleksandr Cherniy <aleksandr.cherniy@gmail.com>
2018-12-07 03:01:43 +0300

    In progress

--- index.php
+++ index.php
@@ -20,20 +20,23 @@
             <div class="general-menu">
                 <ul>
                     <li class="active"><a href="#">Новости</a></li>
-                    <li class=""><a href="#">Рейтинг БК<span></span></a></li>
-                    <li class=""><a href="#">Прогнозы<span></span></a></a></li>
+                    <li class=""><a href="#">Рейтинг БК<i class="ico"><svg width="12px" height="8px"><use xlink:href="/assets/image/svg-symbols.svg#expand_more_24px"></use></svg></object></i></a></li>
+                    <li class=""><a href="#">Прогнозы<i class="ico"><svg width="12px" height="8px"><use xlink:href="/assets/image/svg-symbols.svg#expand_more_24px"></use></svg></i></a></a></li>
                     <li class=""><a href="#">Школа ставок</a></li>
                     <li class=""><a href="#">Бонусы</a></li>
-                    <li class="premium"><a href="#"><span></span>Премиум</a></li>
+                    <li class="premium"><a href="#"><i class="ico"><svg width="16px" height="11px"><use xlink:href="/assets/image/svg-symbols.svg#crown_24px"></use></svg></i>Премиум</a></li>
                 </ul>
             </div>
             <div class="search-bar"></div>
-            <div class="login"></div>
-            <div class="search-button">
-                <a href="#" class="" id="search_button">
+            <div class="search">
+                <a href="javascript:void(0);" class="search-button" id="search_button">
                     <span class="loop"><img src="assets/image/svg/action/search_24px.svg"></span>
                     <span class="cross"><img src="assets/image/svg/content/clear_24px.svg"></span>
                 </a>
+            </div>
+            <div class="login">
+                <div class="login-button button" id="login_button">Вход</div>
+                <div class="logged" id="logged" style="display: none"><img src="" class="userpic"></div>
             </div>
         </div>
     </div>
