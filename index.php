ccc09f50d1b26be51a24b44272b11ea8336a464b
Aleksandr Cherniy <aleksandr.cherniy@gmail.com>
2018-12-12 02:40:32 +0300

    In progress (footer)

--- index.php
+++ index.php
@@ -17,11 +17,11 @@
                     <img src="assets/image/logo/betrating.png" alt="betrating">
                 </a>
             </div>
-            <div class="general-menu">
+            <div class="menu">
                 <ul>
                     <li class="active"><a href="#">Новости</a></li>
-                    <li class=""><a href="#">Рейтинг БК<i class="ico"><svg width="12px" height="8px"><use xlink:href="/assets/image/svg-symbols.svg#expand_more"></use></svg></object></i></a></li>
-                    <li class=""><a href="#">Прогнозы<i class="ico"><svg width="12px" height="8px"><use xlink:href="/assets/image/svg-symbols.svg#expand_more"></use></svg></i></a></a></li>
+                    <li class=""><a href="#">Рейтинг БК<i class="ico"><svg width="12px" height="8px"><use xlink:href="/assets/image/svg-symbols.svg#expand_more"></use></svg></i></a></li>
+                    <li class=""><a href="#">Прогнозы<i class="ico"><svg width="12px" height="8px"><use xlink:href="/assets/image/svg-symbols.svg#expand_more"></use></svg></i></a></li>
                     <li class=""><a href="#">Школа ставок</a></li>
                     <li class=""><a href="#">Бонусы</a></li>
                     <li class="premium"><a href="#"><i class="ico"><svg width="16px" height="11px"><use xlink:href="/assets/image/svg-symbols.svg#crown"></use></svg></i>Премиум</a></li>
@@ -52,7 +52,51 @@
 
 <footer>
     <div class="container">
-        <div class="footer"></div>
+        <div class="footer">
+            <div class="footer-row footer-row-one">
+                <div class="logo">
+                    <img src="assets/image/logo/betrating-grey.png" alt="betrating">
+                </div>
+                <div class="menu">
+                    <ul>
+                        <li class=""><a href="#">Новости</a></li>
+                        <li class=""><a href="#">Рейтинг БК</a></li>
+                        <li class=""><a href="#">Прогнозы</a></li>
+                        <li class=""><a href="#">Школа ставок</a></li>
+                        <li class=""><a href="#">Бонусы</a></li>
+                        <li class="premium"><a href="#"><i class="ico"><svg width="16px" height="11px"><use xlink:href="/assets/image/svg-symbols.svg#crown"></use></svg></i>Премиум</a></li>
+                    </ul>
+                    <div class="pages">
+                        <a href="#">Прогнозы на РПЛ</a>
+                        <a href="#">Прогнозы на Лигу чемпионов</a>
+                        <a href="#">Прогнозы на Лигу Европы</a>
+                    </div>
+                </div>
+                <div class="social">
+                    <a href="#" class="social-item vk"><i class="ico"><svg><use xlink:href="/assets/image/svg-symbols.svg#vk"></use></svg></i></a>
+                    <a href="#" class="social-item tw"><i class="ico"><svg><use xlink:href="/assets/image/svg-symbols.svg#twitter"></use></svg></i></a>
+                    <span class="social-item age">18+</span>
+                </div>
+            </div>
+            <div class="footer-row footer-row-two">
+                <div class="services">
+                    Сетевое издание «Рейтинг букмекерских контор» зарегистрировано в Федеральной службе
+                    по надзору в сфере связи, информационных технологий и массовых коммуникаций (Роскомнадзор)
+                    1 ноября 2017 года. Свидетельство о регистрации ЭЛ № ФС 77 - 71481
+                </div>
+                <div class="services">
+                    Учредитель: ООО "Ленинград Медиа"<br>
+                    Главный редактор: Точилин Д.Н.<br>
+                    Электронная почта редакции: <a href="#">smi@betrating.ru</a>
+                </div>
+            </div>
+            <div class="footer-row footer-row-three">
+                <div class="services" style="margin-right: 10px;"><a href="#">Пользовательское соглашение</a></div>
+                <div class="services" style="margin-right: 68px;"><a href="#">Политика приватности</a></div>
+                <div class="services">© 2015 - 2018 независимый рейтинг букмекеров от betrating.ru</div>
+                <div class="services">Designed by <a href="#">Flatstudio</a></div>
+            </div>
+        </div>
     </div>
 </footer>
 
