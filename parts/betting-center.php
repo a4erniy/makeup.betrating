7e9df2baf2f7ca9804baa77a8790d2414e66afd1
Aleksandr Cherniy <aleksandr.cherniy@gmail.com>
2018-12-11 03:43:30 +0300

    In progress (список прогнозов)

--- parts/betting-center.php
+++ parts/betting-center.php
@@ -1,18 +1,124 @@
 <div class="betting-center">
     <div class="section-title">Беттинг-центр</div>
-    <ul class="nav" role="tablist">
-        <li class="nav-item">
+    <ul class="nav tabs-list" role="tablist">
+        <li class="nav-item tabs-item">
             <a class="" data-toggle="tab" href="#bc_yesterday" role="tab" aria-selected="false" rel="nofollow">Вчера</a>
         </li>
-        <li class="nav-item">
+        <li class="nav-item tabs-item">
             <a class="active" data-toggle="tab" href="#bc_today" role="tab" aria-selected="true" rel="nofollow">Сегодня</a>
         </li>
-        <li class="nav-item">
+        <li class="nav-item tabs-item">
             <a class="" data-toggle="tab" href="#bc_tomorrow" role="tab" aria-selected="false" rel="nofollow">Завтра</a>
         </li>
     </ul>
     <div class="tab-content">
-        <div class="tab-pane fade" id="bc_yesterday" role="tabpanel">1</div>
+        <div class="tab-pane fade" id="bc_yesterday" role="tabpanel">
+            <div class="betting-center-card">
+                <div class="head">
+                    <div class="league">
+                        <div class="logo"><img src="/assets/image/svg/16x16/Sports/football.svg"></div>
+                        <div class="name">Лига чемпионов УЕФА</div>
+                    </div>
+                    <div class="time">22:00</div>
+                </div>
+                <div class="body">
+                    <div class="matches">
+                        <div class="team">
+                            <div class="logo"><img src="/content/team/logo-1.png" alt=""></div>
+                            <div class="name">Барселона</div>
+                            <div class="score">-</div>
+                        </div>
+                        <div class="team">
+                            <div class="logo"><img src="/content/team/logo-2.png" alt=""></div>
+                            <div class="name">Интер</div>
+                            <div class="score">-</div>
+                        </div>
+                    </div>
+                    <a href="#" class="bet">
+                        <div class="type">1</div>
+                        <div class="value">6.51</div>
+                    </a>
+                    <a href="#" class="bet">
+                        <div class="type">X</div>
+                        <div class="value">2.43</div>
+                    </a>
+                    <a href="#" class="bet">
+                        <div class="type">2</div>
+                        <div class="value">3.27</div>
+                    </a>
+                </div>
+            </div>
+            <div class="betting-center-card">
+                <div class="head">
+                    <div class="league">
+                        <div class="logo"><img src="/assets/image/svg/16x16/Sports/football.svg"></div>
+                        <div class="name">Лига чемпионов УЕФА</div>
+                    </div>
+                    <div class="time">22:00</div>
+                </div>
+                <div class="body">
+                    <div class="matches">
+                        <div class="team">
+                            <div class="logo"><img src="/content/team/logo-1.png" alt=""></div>
+                            <div class="name">Барселона</div>
+                            <div class="score">-</div>
+                        </div>
+                        <div class="team">
+                            <div class="logo"><img src="/content/team/logo-2.png" alt=""></div>
+                            <div class="name">Интер</div>
+                            <div class="score">-</div>
+                        </div>
+                    </div>
+                    <a href="#" class="bet">
+                        <div class="type">1</div>
+                        <div class="value">6.51</div>
+                    </a>
+                    <a href="#" class="bet">
+                        <div class="type">X</div>
+                        <div class="value">2.43</div>
+                    </a>
+                    <a href="#" class="bet">
+                        <div class="type">2</div>
+                        <div class="value">3.27</div>
+                    </a>
+                </div>
+            </div>
+            <div class="betting-center-card">
+                <div class="head">
+                    <div class="league">
+                        <div class="logo"><img src="/assets/image/svg/16x16/Sports/football.svg"></div>
+                        <div class="name">Лига чемпионов УЕФА</div>
+                    </div>
+                    <div class="time">22:00</div>
+                </div>
+                <div class="body">
+                    <div class="matches">
+                        <div class="team">
+                            <div class="logo"><img src="/content/team/logo-1.png" alt=""></div>
+                            <div class="name">Барселона</div>
+                            <div class="score">-</div>
+                        </div>
+                        <div class="team">
+                            <div class="logo"><img src="/content/team/logo-2.png" alt=""></div>
+                            <div class="name">Интер</div>
+                            <div class="score">-</div>
+                        </div>
+                    </div>
+                    <a href="#" class="bet">
+                        <div class="type">1</div>
+                        <div class="value">6.51</div>
+                    </a>
+                    <a href="#" class="bet">
+                        <div class="type">X</div>
+                        <div class="value">2.43</div>
+                    </a>
+                    <a href="#" class="bet">
+                        <div class="type">2</div>
+                        <div class="value">3.27</div>
+                    </a>
+                </div>
+            </div>
+        </div>
         <div class="tab-pane fade show active" id="bc_today" role="tabpanel">
             <div class="betting-center-card">
                 <div class="head">
@@ -225,6 +331,182 @@
                 </div>
             </div>
         </div>
-        <div class="tab-pane fade" id="bc_tomorrow" role="tabpanel">3</div>
+        <div class="tab-pane fade" id="bc_tomorrow" role="tabpanel">
+            <div class="betting-center-card">
+                <div class="head">
+                    <div class="league">
+                        <div class="logo"><img src="/assets/image/svg/16x16/Sports/football.svg"></div>
+                        <div class="name">Лига чемпионов УЕФА</div>
+                    </div>
+                    <div class="time">22:00</div>
+                </div>
+                <div class="body">
+                    <div class="matches">
+                        <div class="team">
+                            <div class="logo"><img src="/content/team/logo-1.png" alt=""></div>
+                            <div class="name">Барселона</div>
+                            <div class="score">-</div>
+                        </div>
+                        <div class="team">
+                            <div class="logo"><img src="/content/team/logo-2.png" alt=""></div>
+                            <div class="name">Интер</div>
+                            <div class="score">-</div>
+                        </div>
+                    </div>
+                    <a href="#" class="bet">
+                        <div class="type">1</div>
+                        <div class="value">6.51</div>
+                    </a>
+                    <a href="#" class="bet">
+                        <div class="type">X</div>
+                        <div class="value">2.43</div>
+                    </a>
+                    <a href="#" class="bet">
+                        <div class="type">2</div>
+                        <div class="value">3.27</div>
+                    </a>
+                </div>
+            </div>
+            <div class="betting-center-card">
+                <div class="head">
+                    <div class="league">
+                        <div class="logo"><img src="/assets/image/svg/16x16/Sports/football.svg"></div>
+                        <div class="name">Лига чемпионов УЕФА</div>
+                    </div>
+                    <div class="time">22:00</div>
+                </div>
+                <div class="body">
+                    <div class="matches">
+                        <div class="team">
+                            <div class="logo"><img src="/content/team/logo-1.png" alt=""></div>
+                            <div class="name">Барселона</div>
+                            <div class="score">-</div>
+                        </div>
+                        <div class="team">
+                            <div class="logo"><img src="/content/team/logo-2.png" alt=""></div>
+                            <div class="name">Интер</div>
+                            <div class="score">-</div>
+                        </div>
+                    </div>
+                    <a href="#" class="bet">
+                        <div class="type">1</div>
+                        <div class="value">6.51</div>
+                    </a>
+                    <a href="#" class="bet">
+                        <div class="type">X</div>
+                        <div class="value">2.43</div>
+                    </a>
+                    <a href="#" class="bet">
+                        <div class="type">2</div>
+                        <div class="value">3.27</div>
+                    </a>
+                </div>
+            </div>
+            <div class="betting-center-card">
+                <div class="head">
+                    <div class="league">
+                        <div class="logo"><img src="/assets/image/svg/16x16/Sports/football.svg"></div>
+                        <div class="name">Лига чемпионов УЕФА</div>
+                    </div>
+                    <div class="time">22:00</div>
+                </div>
+                <div class="body">
+                    <div class="matches">
+                        <div class="team">
+                            <div class="logo"><img src="/content/team/logo-1.png" alt=""></div>
+                            <div class="name">Барселона</div>
+                            <div class="score">-</div>
+                        </div>
+                        <div class="team">
+                            <div class="logo"><img src="/content/team/logo-2.png" alt=""></div>
+                            <div class="name">Интер</div>
+                            <div class="score">-</div>
+                        </div>
+                    </div>
+                    <a href="#" class="bet">
+                        <div class="type">1</div>
+                        <div class="value">6.51</div>
+                    </a>
+                    <a href="#" class="bet">
+                        <div class="type">X</div>
+                        <div class="value">2.43</div>
+                    </a>
+                    <a href="#" class="bet">
+                        <div class="type">2</div>
+                        <div class="value">3.27</div>
+                    </a>
+                </div>
+            </div>
+            <div class="betting-center-card">
+                <div class="head">
+                    <div class="league">
+                        <div class="logo"><img src="/assets/image/svg/16x16/Sports/football.svg"></div>
+                        <div class="name">Лига чемпионов УЕФА</div>
+                    </div>
+                    <div class="time">22:00</div>
+                </div>
+                <div class="body">
+                    <div class="matches">
+                        <div class="team">
+                            <div class="logo"><img src="/content/team/logo-1.png" alt=""></div>
+                            <div class="name">Барселона</div>
+                            <div class="score">-</div>
+                        </div>
+                        <div class="team">
+                            <div class="logo"><img src="/content/team/logo-2.png" alt=""></div>
+                            <div class="name">Интер</div>
+                            <div class="score">-</div>
+                        </div>
+                    </div>
+                    <a href="#" class="bet">
+                        <div class="type">1</div>
+                        <div class="value">6.51</div>
+                    </a>
+                    <a href="#" class="bet">
+                        <div class="type">X</div>
+                        <div class="value">2.43</div>
+                    </a>
+                    <a href="#" class="bet">
+                        <div class="type">2</div>
+                        <div class="value">3.27</div>
+                    </a>
+                </div>
+            </div>
+            <div class="betting-center-card">
+                <div class="head">
+                    <div class="league">
+                        <div class="logo"><img src="/assets/image/svg/16x16/Sports/football.svg"></div>
+                        <div class="name">Лига чемпионов УЕФА</div>
+                    </div>
+                    <div class="time">22:00</div>
+                </div>
+                <div class="body">
+                    <div class="matches">
+                        <div class="team">
+                            <div class="logo"><img src="/content/team/logo-1.png" alt=""></div>
+                            <div class="name">Барселона</div>
+                            <div class="score">-</div>
+                        </div>
+                        <div class="team">
+                            <div class="logo"><img src="/content/team/logo-2.png" alt=""></div>
+                            <div class="name">Интер</div>
+                            <div class="score">-</div>
+                        </div>
+                    </div>
+                    <a href="#" class="bet">
+                        <div class="type">1</div>
+                        <div class="value">6.51</div>
+                    </a>
+                    <a href="#" class="bet">
+                        <div class="type">X</div>
+                        <div class="value">2.43</div>
+                    </a>
+                    <a href="#" class="bet">
+                        <div class="type">2</div>
+                        <div class="value">3.27</div>
+                    </a>
+                </div>
+            </div>
+        </div>
     </div>
 </div>
