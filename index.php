05b69e692fd176cfe7d6df68984231cffed7ab68
Aleksandr Cherniy <aleksandr.cherniy@gmail.com>
2018-12-11 21:42:06 +0300

    In progress (список бонусов)

--- index.php
+++ index.php
@@ -50,9 +50,234 @@
 <?php include_once 'parts/banner-section.php'; ?>
 <?php include_once 'parts/news-section.php'; ?>
 
+<section class="bonuses-section">
+    <div class="container">
+        <div class="section-header">
+            <div class="section-title"><h2>Бонусы и акции от букмекеров</h2></div>
+        </div>
+        <div class="bonuses-list clearfix">
+            <div class="bonus-card">
+                <div class="image">
+                    <img src="content/bonuses/1.jpg" alt="">
+                </div>
+                <div class="company">
+                    <a href="#">
+                        <img src="content/company/leon.png" alt="">
+                    </a>
+                </div>
+                <div class="title">
+                    <a href="#">
+                        <h3>Акция от БК «Фонбет»: 500 рублей за прогноз на бой Кормье — Льюис</h3>
+                    </a>
+                </div>
+                <div class="countdown">
+                    <div class="text">Окончание через:</div>
+                    <div class="timer">
+                        <div class="days">
+                            <span>0</span>
+                            <span>0</span>
+                            <div>дней</div>
+                        </div>
+                        <div class="hours">
+                            <span>0</span>
+                            <span>0</span>
+                            <div>часов</div>
+                        </div>
+                        <div class="minutes">
+                            <span>0</span>
+                            <span>0</span>
+                            <div>минут</div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="bonus-card">
+                <div class="image">
+                    <img src="content/bonuses/1.jpg" alt="">
+                </div>
+                <div class="company">
+                    <a href="#">
+                        <img src="content/company/leon.png" alt="">
+                    </a>
+                </div>
+                <div class="title">
+                    <a href="#">
+                        <h3>Акция от БК «Фонбет»: 500 рублей за прогноз на бой Кормье — Льюис</h3>
+                    </a>
+                </div>
+                <div class="countdown">
+                    <div class="text">Окончание через:</div>
+                    <div class="timer">
+                        <div class="days">
+                            <span>0</span>
+                            <span>0</span>
+                            <div>дней</div>
+                        </div>
+                        <div class="hours">
+                            <span>0</span>
+                            <span>0</span>
+                            <div>часов</div>
+                        </div>
+                        <div class="minutes">
+                            <span>0</span>
+                            <span>0</span>
+                            <div>минут</div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="bonus-card">
+                <div class="image">
+                    <img src="content/bonuses/1.jpg" alt="">
+                </div>
+                <div class="company">
+                    <a href="#">
+                        <img src="content/company/leon.png" alt="">
+                    </a>
+                </div>
+                <div class="title">
+                    <a href="#">
+                        <h3>Акция от БК «Фонбет»: 500 рублей за прогноз на бой Кормье — Льюис</h3>
+                    </a>
+                </div>
+                <div class="countdown">
+                    <div class="text">Окончание через:</div>
+                    <div class="timer">
+                        <div class="days">
+                            <span>0</span>
+                            <span>0</span>
+                            <div>дней</div>
+                        </div>
+                        <div class="hours">
+                            <span>0</span>
+                            <span>0</span>
+                            <div>часов</div>
+                        </div>
+                        <div class="minutes">
+                            <span>0</span>
+                            <span>0</span>
+                            <div>минут</div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="bonus-card">
+                <div class="image">
+                    <img src="content/bonuses/1.jpg" alt="">
+                </div>
+                <div class="company">
+                    <a href="#">
+                        <img src="content/company/leon.png" alt="">
+                    </a>
+                </div>
+                <div class="title">
+                    <a href="#">
+                        <h3>Акция от БК «Фонбет»: 500 рублей за прогноз на бой Кормье — Льюис</h3>
+                    </a>
+                </div>
+                <div class="countdown">
+                    <div class="text">Окончание через:</div>
+                    <div class="timer">
+                        <div class="days">
+                            <span>0</span>
+                            <span>0</span>
+                            <div>дней</div>
+                        </div>
+                        <div class="hours">
+                            <span>0</span>
+                            <span>0</span>
+                            <div>часов</div>
+                        </div>
+                        <div class="minutes">
+                            <span>0</span>
+                            <span>0</span>
+                            <div>минут</div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="bonus-card banner-card">
+                <a href="#">
+                    <img src="content/banner-gusev.jpg" alt="">
+                </a>
+            </div>
+            <div class="bonus-card">
+                <div class="image">
+                    <img src="content/bonuses/1.jpg" alt="">
+                </div>
+                <div class="company">
+                    <a href="#">
+                        <img src="content/company/leon.png" alt="">
+                    </a>
+                </div>
+                <div class="title">
+                    <a href="#">
+                        <h3>Акция от БК «Фонбет»: 500 рублей за прогноз на бой Кормье — Льюис</h3>
+                    </a>
+                </div>
+                <div class="countdown">
+                    <div class="text">Окончание через:</div>
+                    <div class="timer">
+                        <div class="days">
+                            <span>0</span>
+                            <span>0</span>
+                            <div>дней</div>
+                        </div>
+                        <div class="hours">
+                            <span>0</span>
+                            <span>0</span>
+                            <div>часов</div>
+                        </div>
+                        <div class="minutes">
+                            <span>0</span>
+                            <span>0</span>
+                            <div>минут</div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="bonus-card">
+                <div class="image">
+                    <img src="content/bonuses/1.jpg" alt="">
+                </div>
+                <div class="company">
+                    <a href="#">
+                        <img src="content/company/leon.png" alt="">
+                    </a>
+                </div>
+                <div class="title">
+                    <a href="#">
+                        <h3>Акция от БК «Фонбет»: 500 рублей за прогноз на бой Кормье — Льюис</h3>
+                    </a>
+                </div>
+                <div class="countdown">
+                    <div class="text">Окончание через:</div>
+                    <div class="timer">
+                        <div class="days">
+                            <span>0</span>
+                            <span>0</span>
+                            <div>дней</div>
+                        </div>
+                        <div class="hours">
+                            <span>0</span>
+                            <span>0</span>
+                            <div>часов</div>
+                        </div>
+                        <div class="minutes">
+                            <span>0</span>
+                            <span>0</span>
+                            <div>минут</div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="section-more">
+            <a href="#" class="button button-grey">Все бонусы и акции</a>
+        </div>
+    </div>
+</section>
 
-
-<section class="bonuses-section"></section>
 <section class="stavkatv-section"></section>
 
 <footer></footer>
