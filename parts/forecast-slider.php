2000dda17b0e47a464a8c37acad28adf45798bbc
a_cherniy <aleksandr.cherniy@gmail.com>
2018-12-12 15:05:45 +0300

    In progress

--- parts/forecast-slider.php
+++ parts/forecast-slider.php
@@ -10,49 +10,81 @@
                 <div class="title">Барселона — Интер</div>
                 <div class="date">24 октября, 22:00</div>
             </div>
-            <div class="more">Читать прогноз <div class="circle"><svg><use xlink:href="/assets/image/svg-symbols.svg#arrow_forward"></use></svg></div></div>
+            <div class="more">
+                <span>Читать прогноз</span>
+                <div class="circle">
+                    <svg><use xlink:href="/assets/image/svg-symbols.svg#arrow_forward"></use></svg>
+                    <svg><use xlink:href="/assets/image/svg-symbols.svg#arrow_forward"></use></svg>
+                    <svg><use xlink:href="/assets/image/svg-symbols.svg#arrow_forward"></use></svg>
+                    <svg><use xlink:href="/assets/image/svg-symbols.svg#arrow_forward"></use></svg>
+                </div>
+            </div>
         </a>
     </div>
-    <div class="item">
-        <a href="#">
-            <div class="image">
-                <img src="content/forecast/2.jpg" alt="">
-            </div>
-            <div class="mask"></div>
-            <div class="desc">
-                <span>Прогноз на матч:</span>
-                <div class="title">Рома — Валенсия</div>
-                <div class="date">28 октября, 21:00</div>
-            </div>
-            <div class="more">Читать прогноз <div class="circle"><svg><use xlink:href="/assets/image/svg-symbols.svg#arrow_forward"></use></svg></div></div>
-        </a>
-    </div>
-    <div class="item">
-        <a href="#">
-            <div class="image">
-                <img src="content/forecast/3.jpg" alt="">
-            </div>
-            <div class="mask"></div>
-            <div class="desc">
-                <span>Прогноз на матч:</span>
-                <div class="title">Барселона — Интер</div>
-                <div class="date">24 октября, 22:00</div>
-            </div>
-            <div class="more">Читать прогноз <div class="circle"><svg><use xlink:href="/assets/image/svg-symbols.svg#arrow_forward"></use></svg></div></div>
-        </a>
-    </div>
-    <div class="item">
-        <a href="#">
-            <div class="image">
-                <img src="content/forecast/4.jpg" alt="">
-            </div>
-            <div class="mask"></div>
-            <div class="desc">
-                <span>Прогноз на матч:</span>
-                <div class="title">Рома — Валенсия</div>
-                <div class="date">28 октября, 21:00</div>
-            </div>
-            <div class="more">Читать прогноз <div class="circle"><svg><use xlink:href="/assets/image/svg-symbols.svg#arrow_forward"></use></svg></div></div>
-        </a>
-    </div>
+<!--    <div class="item">-->
+<!--        <a href="#">-->
+<!--            <div class="image">-->
+<!--                <img src="content/forecast/2.jpg" alt="">-->
+<!--            </div>-->
+<!--            <div class="mask"></div>-->
+<!--            <div class="desc">-->
+<!--                <span>Прогноз на матч:</span>-->
+<!--                <div class="title">Рома — Валенсия</div>-->
+<!--                <div class="date">28 октября, 21:00</div>-->
+<!--            </div>-->
+<!--            <div class="more">-->
+<!--                <span>Читать прогноз</span>-->
+<!--                <div class="circle">-->
+<!--                    <svg><use xlink:href="/assets/image/svg-symbols.svg#arrow_forward"></use></svg>-->
+<!--                    <svg><use xlink:href="/assets/image/svg-symbols.svg#arrow_forward"></use></svg>-->
+<!--                    <svg><use xlink:href="/assets/image/svg-symbols.svg#arrow_forward"></use></svg>-->
+<!--                    <svg><use xlink:href="/assets/image/svg-symbols.svg#arrow_forward"></use></svg>-->
+<!--                </div>-->
+<!--            </div>-->
+<!--        </a>-->
+<!--    </div>-->
+<!--    <div class="item">-->
+<!--        <a href="#">-->
+<!--            <div class="image">-->
+<!--                <img src="content/forecast/3.jpg" alt="">-->
+<!--            </div>-->
+<!--            <div class="mask"></div>-->
+<!--            <div class="desc">-->
+<!--                <span>Прогноз на матч:</span>-->
+<!--                <div class="title">Барселона — Интер</div>-->
+<!--                <div class="date">24 октября, 22:00</div>-->
+<!--            </div>-->
+<!--            <div class="more">-->
+<!--                <span>Читать прогноз</span>-->
+<!--                <div class="circle">-->
+<!--                    <svg><use xlink:href="/assets/image/svg-symbols.svg#arrow_forward"></use></svg>-->
+<!--                    <svg><use xlink:href="/assets/image/svg-symbols.svg#arrow_forward"></use></svg>-->
+<!--                    <svg><use xlink:href="/assets/image/svg-symbols.svg#arrow_forward"></use></svg>-->
+<!--                    <svg><use xlink:href="/assets/image/svg-symbols.svg#arrow_forward"></use></svg>-->
+<!--                </div>-->
+<!--            </div>-->
+<!--        </a>-->
+<!--    </div>-->
+<!--    <div class="item">-->
+<!--        <a href="#">-->
+<!--            <div class="image">-->
+<!--                <img src="content/forecast/4.jpg" alt="">-->
+<!--            </div>-->
+<!--            <div class="mask"></div>-->
+<!--            <div class="desc">-->
+<!--                <span>Прогноз на матч:</span>-->
+<!--                <div class="title">Рома — Валенсия</div>-->
+<!--                <div class="date">28 октября, 21:00</div>-->
+<!--            </div>-->
+<!--            <div class="more">-->
+<!--                <span>Читать прогноз</span>-->
+<!--                <div class="circle">-->
+<!--                    <svg><use xlink:href="/assets/image/svg-symbols.svg#arrow_forward"></use></svg>-->
+<!--                    <svg><use xlink:href="/assets/image/svg-symbols.svg#arrow_forward"></use></svg>-->
+<!--                    <svg><use xlink:href="/assets/image/svg-symbols.svg#arrow_forward"></use></svg>-->
+<!--                    <svg><use xlink:href="/assets/image/svg-symbols.svg#arrow_forward"></use></svg>-->
+<!--                </div>-->
+<!--            </div>-->
+<!--        </a>-->
+<!--    </div>-->
 </div>
